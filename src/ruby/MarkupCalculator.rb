30105410ef6feab447f711413cfd2bbe7a3af44d
Steve Page <page.steven.c@gmail.com>
2016-08-14 22:55:02 -0400

    add ability to change base markup

--- src/ruby/MarkupCalculator.rb
+++ src/ruby/MarkupCalculator.rb
@@ -2,8 +2,20 @@ class MarkupCalculator
 
 	@@base = 0.05
 	
-	def calculate(initialAmount)
+	def estimate(initialAmount)
 		initialAmount + (@@base * initialAmount)
+	end
+	
+	def calculateBaseMarkup(amount)
+		@@base * amount
+	end
+	
+	def setBaseMarkup(percent)
+		@@base = percent / 100.0
+	end
+	
+	def getBaseMarkup
+		@@base
 	end
 	
 end
--- src/ruby/TestSuite.rb
+++ src/ruby/TestSuite.rb
@@ -4,28 +4,40 @@ class TestSuite
 
 	def initialize
 		@calculator = MarkupCalculator.new
-		@totalTests = 1
+		@calculator.setBaseMarkup(10)
+		@totalTests = 0
 		@results = []
 	end
 
 	def run_all
 		testnum = 0
-		@results[testnum+=1] = baseMarkupApplied
+		@results[testnum+=1] = calculateBaseMarkup_ExactDollarResult
+		@results[testnum+=1] = calculateBaseMarkup_ExactCents
+		@results[testnum+=1] = testSetBaseMarkup
+		@totalTests = testnum
 		reportResults
 	end
 	
 	def reportResults
 		passes = 0
 		@results.each do |aresult|
-			if aresult 
+			if aresult
 				passes += 1 
 			end
 		end
 		puts 'Total tests = ' + @totalTests.to_s + ', Passes = ' + passes.to_s + ', Fails = ' + (@totalTests - passes).to_s
 	end
 	
-	def baseMarkupApplied
-		105 == @calculator.calculate(100)
+	def testSetBaseMarkup
+		0.1 == @calculator.getBaseMarkup
+	end
+	
+	def calculateBaseMarkup_ExactDollarResult
+		10 == @calculator.calculateBaseMarkup(100)
+	end
+	
+	def calculateBaseMarkup_ExactCents
+		10.5 == @calculator.calculateBaseMarkup(105)
 	end
 	
 end
